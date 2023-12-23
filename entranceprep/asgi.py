12bd68c15e4779d94f1acf4829dda1367213921d
Dipendra km <dipendrakm703@gmail.com>
2023-12-22 19:52:08 -0500

    all7

--- entranceprep/asgi.py
+++ entranceprep/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for entraceprep project.
+ASGI config for entranceprep project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entraceprep.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entranceprep.settings')
 
 application = get_asgi_application()
--- entranceprep/wsgi.py
+++ entranceprep/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for entraceprep project.
+WSGI config for entranceprep project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entraceprep.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entranceprep.settings')
 
 application = get_wsgi_application()
